eb8477be261cf248dc19f65c77d504bccd2aee63
IvanVukorepa <ivan.vukorepa132@gmail.com>
2018-03-26 10:13:23 +0200

    Prvi_Commit

---
+++ mmandi01_Vj_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mmandi01_Vj_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //neke izmjene
+        }
+    }
+}
